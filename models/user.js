c39b512c639b7e4d4ab5640064c710accadcc231
Stephen Bucknall <>
2022-11-23 16:06:35 +0800

    Added oneHR Website API

---
+++ models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  url: { type: String, required: true },
+  createdAt: Date,
+  updatedAt: Date,
+});
+
+module.exports = mongoose.model('User', userSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,73 +1,182 @@
+const { response } = require('express');
 var express = require('express');
 var Task = require('../models/task');
+var User = require('../models/user');
 
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   Task.find()
-    .then((tasks) => {      
+    .then(tasks => {
       const currentTasks = tasks.filter(task => !task.completed);
       const completedTasks = tasks.filter(task => task.completed === true);
 
-      console.log(`Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`)
-      res.render('index', { currentTasks: currentTasks, completedTasks: completedTasks });
+      console.log(
+        `Total tasks: ${tasks.length}   Current tasks: ${currentTasks.length}    Completed tasks:  ${completedTasks.length}`
+      );
+      res.render('index', {
+        currentTasks: currentTasks,
+        completedTasks: completedTasks,
+      });
     })
-    .catch((err) => {
+    .catch(err => {
       console.log(err);
       res.send('Sorry! Something went wrong.');
     });
 });
 
-
-router.post('/addTask', function(req, res, next) {
+router.post('/addTask', function (req, res, next) {
   const taskName = req.body.taskName;
   const createDate = Date.now();
-  
+
   var task = new Task({
     taskName: taskName,
-    createDate: createDate
+    createDate: createDate,
   });
-  console.log(`Adding a new task ${taskName} - createDate ${createDate}`)
+  console.log(`Adding a new task ${taskName} - createDate ${createDate}`);
 
-  task.save()
-      .then(() => { 
-        console.log(`Added new task ${taskName} - createDate ${createDate}`)        
-        res.redirect('/'); })
-      .catch((err) => {
-          console.log(err);
-          res.send('Sorry! Something went wrong.');
-      });
+  task
+    .save()
+    .then(() => {
+      console.log(`Added new task ${taskName} - createDate ${createDate}`);
+      res.redirect('/');
+    })
+    .catch(err => {
+      console.log(err);
+      res.send('Sorry! Something went wrong.');
+    });
 });
 
-router.post('/completeTask', function(req, res, next) {
-  console.log("I am in the PUT method")
+router.post('/completeTask', function (req, res, next) {
+  console.log('I am in the PUT method');
   const taskId = req.body._id;
   const completedDate = Date.now();
 
-  Task.findByIdAndUpdate(taskId, { completed: true, completedDate: Date.now()})
-    .then(() => { 
-      console.log(`Completed task ${taskId}`)
-      res.redirect('/'); }  )
-    .catch((err) => {
+  Task.findByIdAndUpdate(taskId, { completed: true, completedDate: Date.now() })
+    .then(() => {
+      console.log(`Completed task ${taskId}`);
+      res.redirect('/');
+    })
+    .catch(err => {
       console.log(err);
       res.send('Sorry! Something went wrong.');
     });
 });
 
-
-router.post('/deleteTask', function(req, res, next) {
+router.post('/deleteTask', function (req, res, next) {
   const taskId = req.body._id;
   const completedDate = Date.now();
   Task.findByIdAndDelete(taskId)
-    .then(() => { 
-      console.log(`Deleted task $(taskId)`)      
-      res.redirect('/'); }  )
-    .catch((err) => {
+    .then(() => {
+      console.log(`Deleted task $(taskId)`);
+      res.redirect('/');
+    })
+    .catch(err => {
       console.log(err);
       res.send('Sorry! Something went wrong.');
     });
 });
 
+// oneHR Website API
+const checkApiKeyIsValid = (req, res, next) => {
+  const apiKey = req.headers['x-api-key'];
+  const xApiKey = process.env.X_API_Key;
+
+  if (apiKey !== xApiKey) {
+    console.error('401 Invalid  header: x-api-key');
+    return res.status(401).send('Invalid  header: x-api-key');
+  }
+
+  next();
+};
+
+router.get('/api/users', (req, res, next) => {
+  const { email, url } = req.query;
+  const query = {};
+
+  if (email) {
+    query.email = email;
+  }
+  if (url) {
+    query.url = url;
+  }
+
+  User.find(query)
+    .then(users => {
+      res.send(users);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(err);
+    });
+});
+
+router.get('/api/users/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  User.findById(id)
+    .then(user => {
+      res.send(user);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(err);
+    });
+});
+
+router.post('/api/users', checkApiKeyIsValid, (req, res, next) => {
+  const users = req.body;
+  const now = Date.now();
+
+  if (Array.isArray(users)) {
+    users.forEach(user => {
+      user.createdAt = now;
+      user.updatedAt = now;
+    });
+  } else {
+    users.createdAt = now;
+    users.updatedAt = now;
+  }
+
+  User.create(users)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(err);
+    });
+});
+
+router.put('/api/users/:id', checkApiKeyIsValid, (req, res, next) => {
+  const { id } = req.params;
+  const user = req.body;
+  const now = Date.now();
+
+  user.updatedAt = now;
+
+  User.findByIdAndUpdate(id, user)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(err);
+    });
+});
+
+router.delete('/api/users/:id', checkApiKeyIsValid, (req, res, next) => {
+  const { id } = req.params;
+
+  User.findByIdAndDelete(id)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send(err);
+    });
+});
 
 module.exports = router;
